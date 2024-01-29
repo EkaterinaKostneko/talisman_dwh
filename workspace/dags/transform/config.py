1858a4e54f83d8281efed05a5e413ef79007d752
kostenko <kostenko.ekaterina@gmail.com>
2024-01-29 20:28:57 +0900

    transform dir added

---
+++ workspace/dags/transform/config.py
@@ -0,0 +1,9 @@
+log_level = 'info' # 'error', 'warning', 'info', 'debug'
+dwh_db_conn = 'dwh_apteka'
+src_dwh_db_conn = 'dwh_apteka'
+project_dir = 'transform'
+
+min_load_date = '2020-01-01'
+max_load_date = '2099-01-01'
+inc_start = '2022-01-01'
+inc_end = '2099-01-01'
