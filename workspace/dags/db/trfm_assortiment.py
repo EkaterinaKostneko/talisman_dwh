e7306f4052bd6f673ad80ca7c0674bc2dc94788e
kostenko <kostenko.ekaterina@gmail.com>
2024-02-01 00:13:37 +0900

    refactoring

--- workspace/dags/db/trfm_assortiment.py
+++ workspace/dags/db/trfm_assortiment.py
@@ -2,9 +2,9 @@
 import datetime as dt
 from airflow import DAG
 
-from trfm.helpers import (run_sql, finish_load, extract_sql, run_python, get_load_params)
-from trfm.config import dwh_db_conn
-from trfm.config import src_dwh_db_conn
+from db.helpers import (run_sql, finish_load, extract_sql, run_python, get_load_params)
+from db.config import dwh_db_conn
+from db.config import src_dwh_db_conn
 
 default_args = {
     'owner': 'airflow',
@@ -16,17 +16,17 @@ default_args = {
     'retries': 0
 }
 
-dag_id = 'trfm_assortiment_plain'
+dag_id = 'trfm_assortiment'
 
 # заменить подключение к источнику и поменять схему продюсера на схему источника
 def t_extract_sql(entity):
     return extract_sql(
         source_conn=src_dwh_db_conn,
-        subdir='trfm',
+        subdir='db',
         entity=entity,
         params={
-            'AF_DWH_DB_SCHEMA_PRODUCER': 'stg_dwh',
-            'AF_DWH_DB_SCHEMA_CONSUMER': 'core'
+            'AF_DWH_DB_SCHEMA_PRODUCER': 'dbo',
+            'AF_DWH_DB_SCHEMA_CONSUMER': 'stg_dwh'
         }
     )
 
@@ -36,8 +36,11 @@ with DAG(dag_id, default_args=default_args, schedule_interval='0 1 * * *', catch
   #  t_truncate = run_sql( script='truncate_retail_reports.sql',  task_id='truncate_stg_dwh')
     t_finish_load = finish_load()
     t_trfm_core = run_sql(script='core_assortiment_plain.sql', task_id='core_plain')
-    t_trfm_step1 = run_sql(script='trfm_assortiment_plain_step1.sql', task_id='trfm_step1')
+    t_trfm_step1 = run_sql(script='trfm_assortiment_add_return.sql', task_id='trfm_add_returns')
+    t_trfm_step2 = run_sql(script='trfm_assortiment_step2.sql', task_id='trfm_simpl_discont')
+    t_trfm_step3 = run_sql(script='trfm_assortiment_step3.sql', task_id='trfm_union_returns')
+    t_trfm_final = run_sql(script='trfm_assortiment_final.sql', task_id='trfm_filter_resort')
     t_get_load_params = get_load_params()
 
 
-t_get_load_params >> t_trfm_core >> t_trfm_step1 >> t_finish_load
+t_get_load_params >> t_trfm_core >> t_trfm_step1 >> [t_trfm_step2, t_trfm_step3, t_trfm_final] >> t_finish_load
--- workspace/source/db/sql/trfm_assortiment_add_return.sql
+++ workspace/source/db/sql/trfm_assortiment_add_return.sql
@@ -1,4 +1,21 @@
-TRUNCATE TABLE core.assortiment_plain_add_return;
+DROP TABLE IF EXISTS core.assortiment_plain_add_return CASCADE;
+
+CREATE table core.assortiment_plain_add_return
+AS
+SELECT *
+FROM core.assortiment_plain
+WHERE 1=0;
+ALTER TABLE core.assortiment_plain_add_return RENAME COLUMN discount1 TO discount;
+ALTER TABLE core.assortiment_plain_add_return DROP COLUMN discount2;
+ALTER TABLE core.assortiment_plain_add_return DROP COLUMN discount3;
+ALTER TABLE core.assortiment_plain_add_return DROP COLUMN discount4;
+ALTER TABLE core.assortiment_plain_add_return DROP COLUMN discount5;
+
+CREATE INDEX assortiment_plain_docdate_idx2 ON core.assortiment_plain_add_return USING btree (docdate);
+CREATE INDEX assortiment_plain_iddoc_idx2 ON core.assortiment_plain_add_return USING btree (iddoc);
+CREATE INDEX assortiment_plain_iditem_idx2 ON core.assortiment_plain_add_return USING btree (iditem);
+CREATE INDEX assortiment_plain_idstore_idx2 ON core.assortiment_plain_add_return USING btree (idstore);
+CREATE INDEX assortiment_plain_updatedate_idx2 ON core.assortiment_plain_add_return USING btree (updatedate);
 
 INSERT INTO core.assortiment_plain_add_return
 SELECT
@@ -13,11 +30,12 @@ SELECT
     move_quantity ,
     -revenue/quantity*return_quantity  AS revenue,
     -purchase/quantity*return_quantity AS purchase ,
-    -(discount1 +
-    discount2 +
-    discount3 +
-    discount4 +
-    discount5)/quantity*return_quantity AS discount,
+--    -(discount1 +
+--    discount2 +
+--    discount3 +
+--    discount4 +
+--    discount5)/quantity*return_quantity AS discount,
+    0 as discount,
     manufacture ,
     kind
 FROM
