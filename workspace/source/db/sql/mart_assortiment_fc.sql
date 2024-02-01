a4510dcb7ca041bda6af51137e299d8fa7333681
kostenko <kostenko.ekaterina@gmail.com>
2024-02-01 19:02:07 +0900

    final view assortiment

---
+++ workspace/source/db/sql/mart_assortiment_fc.sql
@@ -0,0 +1,18 @@
+TRUNCATE TABLE marts.mart_assortiment_sales;
+
+INSERT INTO marts.mart_assortiment_sales
+(
+	"Дата",
+ 	"ID аптеки",
+ 	"ID товара",
+ 	"Количество",
+ 	"Валовая выручка",
+ 	"Валовая прибыль",
+ 	"Цена розничная",
+ 	"Прибыль с упаковки",
+ 	"Back маржа",
+ 	"Общая прибыль с упаковки")
+SELECT
+
+FROM core.
+;
