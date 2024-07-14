903dfd3ae832576a246b2a5a2a0845a29202ce3e
kostenko <kostenko.ekaterina@gmail.com>
2024-07-14 20:38:37 +0900

    fix latest

--- workspace/source/central/sql/actual/marts.sql
+++ workspace/source/central/sql/actual/marts.sql
@@ -119,38 +119,6 @@ ch.Status = 1 AND
  ch.WriteOffFlag = 0
 group by DocDate,
 PharmacyCode
-
-
-drop view if exists marts.latest ;
-
-create or replace view marts.latest
-as
-(select 'checktables' 	"table", docdate "date"
-from ods.checktables c
-order by c.docdate desc limit 1)
-union all
-(select 'checkheaders' 	"table", docdate "date"
-from ods.checkheaders c
-order by c.docdate desc limit 1)
-union all
-(select 'total results'  "table", c.docdate  "date"
-from marts.mart_total_results_by_store  c
-order by c.docdate desc limit 1)
-union all
-(select 'mart_assortiment_ce'  "table", c.docdate  "date"
-from marts.mart_assortiment_ce c
-order by c.docdate desc limit 1)
-union all
-(select 'margin_project'  "table", c."Дата" "date"
-from marts.mart_margin_projects c
-order by c."Дата"  desc limit 1)
-union all
-(select 'delivery_goods'  "table", c."Дата оформления"::date "date"
-from marts.mart_delivery_goods c
-order by c."Дата оформления"::date  desc limit 1)
-union all
-(select 'sales_db'  "table", c.docdate  "date"
-from dds.sales_db c
-order by c.docdate  desc limit 1)
-
 ;
+
+refresh materialized view marts.latest ;
